6fdac056282fbcb12f4de71d1068d79487242ecf
Mictronics <github@mictronics.de>
2020-08-17 19:12:42 +0200

    Simplified code.

--- ais-simulator.py
+++ ais-simulator.py
@@ -43,70 +43,38 @@ class top_block(gr.top_block):
     def __init__(self, p, c, amp, lna, sr, br, ppm):
         gr.top_block.__init__(self, 'AIS Simulator')
 
-        ##################################################
-        # Variables
-        ##################################################
-        self.samp_rate = samp_rate = sr
-        self.channel_select = c
-        self.bit_rate = bit_rate = br
-
-        ##################################################
         # Blocks
-        ##################################################
-        self.osmosdr_sink_0 = osmosdr.sink(args="numchan=" + str(1) + " " + '')
-        self.osmosdr_sink_0.set_sample_rate(samp_rate)
-        self.osmosdr_sink_0.set_freq_corr(ppm, 0)
-        self.osmosdr_sink_0.set_center_freq(161975000 + 50000 * c, 0)
-        self.osmosdr_sink_0.set_gain(14 if amp else 0, 0)
-        self.osmosdr_sink_0.set_if_gain(lna, 0)
-        self.osmosdr_sink_0.set_bb_gain(16, 0)
-        self.osmosdr_sink_0.set_antenna("", 0)
-        self.digital_gmsk_mod_0 = digital.gmsk_mod(
-            samples_per_symbol=int(samp_rate / bit_rate),
+        osmosdr_sink_0 = osmosdr.sink(args="numchan=" + str(1) + " " + '')
+        osmosdr_sink_0.set_sample_rate(sr)
+        osmosdr_sink_0.set_freq_corr(ppm, 0)
+        osmosdr_sink_0.set_center_freq(161975000 + 50000 * c, 0)
+        osmosdr_sink_0.set_gain(14 if amp else 0, 0)
+        osmosdr_sink_0.set_if_gain(lna, 0)
+        osmosdr_sink_0.set_bb_gain(16, 0)
+        osmosdr_sink_0.set_antenna("", 0)
+        digital_gmsk_mod_0 = digital.gmsk_mod(
+            samples_per_symbol=int(sr / br),
             bt=0.4,
             verbose=False,
             log=False,
         )
-        self.blocks_socket_pdu_0 = blocks.socket_pdu('TCP_SERVER', '', '1337', 1024, False)
-        self.blocks_pdu_to_tagged_stream_0 = blocks.pdu_to_tagged_stream(blocks.byte_t, 'packet_len')
-        self.blocks_multiply_const_vxx_0 = blocks.multiply_const_vcc((0.9, ))
-        self.ais_simulator_build_frame_0 = ais_simulator.build_frame(p, True, True, 'packet_len')
+        blocks_socket_pdu_0 = blocks.socket_pdu('TCP_SERVER', 'localhost', '1337', 1024, False)
+        blocks_pdu_to_tagged_stream_0 = blocks.pdu_to_tagged_stream(blocks.byte_t, 'packet_len')
+        blocks_multiply_const_vxx_0 = blocks.multiply_const_vcc((0.9, ))
+        ais_build_frame = ais_simulator.build_frame(p, True, True, 'packet_len')
 
-        ##################################################
         # Connections
-        ##################################################
-        self.msg_connect((self.blocks_socket_pdu_0, 'pdus'), (self.blocks_pdu_to_tagged_stream_0, 'pdus'))
-        self.connect((self.blocks_pdu_to_tagged_stream_0, 0), (self.ais_simulator_build_frame_0, 0))
-        self.connect((self.ais_simulator_build_frame_0, 0), (self.digital_gmsk_mod_0, 0))
-        self.connect((self.digital_gmsk_mod_0, 0), (self.blocks_multiply_const_vxx_0, 0))
-        self.connect((self.blocks_multiply_const_vxx_0, 0), (self.osmosdr_sink_0, 0))
-
-    def get_samp_rate(self):
-        return self.samp_rate
-
-    def set_samp_rate(self, samp_rate):
-        self.samp_rate = samp_rate
-        self.osmosdr_sink_0.set_samp_rate(self.samp_rate)
-
-    def get_channel_select(self):
-        return self.channel_select
-
-    def set_channel_select(self, channel_select):
-        self.channel_select = channel_select
-        self.analog_sig_source_x_0.set_frequency(-25000 + 50000 * self.channel_select)
-
-    def get_bit_rate(self):
-        return self.bit_rate
-
-    def set_bit_rate(self, bit_rate):
-        self.bit_rate = bit_rate
+        self.msg_connect((blocks_socket_pdu_0, 'pdus'), (blocks_pdu_to_tagged_stream_0, 'pdus'))
+        self.connect((blocks_pdu_to_tagged_stream_0, 0), (ais_build_frame, 0))
+        self.connect((ais_build_frame, 0), (digital_gmsk_mod_0, 0))
+        self.connect((digital_gmsk_mod_0, 0), (blocks_multiply_const_vxx_0, 0))
+        self.connect((blocks_multiply_const_vxx_0, 0), (osmosdr_sink_0, 0))
 
 
 if __name__ == '__main__':
     def signal_handler(signal, frame):
         tb.stop()
         tb.wait()
-        del tb.ais_simulator_build_frame_0
         sys.exit(0)
 
     desc = """GnuRadio AIS Transmitter. Copyright Embyte & Pastus, Mictronics 2013-2020."""
