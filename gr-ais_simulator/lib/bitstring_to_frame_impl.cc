96b5833702a0c4e138e8a17a7e524101ca747aa1
Mictronics <github@mictronics.de>
2022-01-25 19:06:37 +0100

    Fix memory out of bound access. Fix #3

--- gr-ais_simulator/lib/bitstring_to_frame_impl.cc
+++ gr-ais_simulator/lib/bitstring_to_frame_impl.cc
@@ -225,7 +225,7 @@ namespace gr
         char *bitstring_to_frame_impl::int2bin(int a, char *buffer, int buf_size)
         {
             buffer += (buf_size - 1);
-            for (int i = 31; i >= 0; i--)
+            for (int i = buf_size - 1; i >= 0; i--)
             {
                 *buffer-- = (a & 1) + '0';
                 a >>= 1;
